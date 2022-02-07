68f79843773710437d34e7336e6739a4d31c4527
daniel <danydu1299@gmail.com>
2022-02-07 12:00:57 -0500

    add

--- level_1/main.py
+++ level_1/main.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 """This module use request and beautifulsoup library"""
 
-from bs4 import BeautifulSoup
+
 import requests
+
 
 URL = "http://158.69.76.135/level1.php"
 
 with requests.session() as session:
-    """With with let's keep the session open"""
-    for i in range(4096):
-        """We need the loop here like global to obtain
+    """With with let's keep the session open
+    We need the loop here like global to obtain
         each time the token code to vote"""
+    for i in range(4096):
 
         response = session.get(URL)
         token = res.cookies["holdthedoor"]
         credentials = {"id": 3811, "holdthedoor": "submit", "key": token}
 
         vote = session.post(URL, data=credentials)
-
-    print("Vote: {} -- status: {}".format(i + 1, vote.status_code))
